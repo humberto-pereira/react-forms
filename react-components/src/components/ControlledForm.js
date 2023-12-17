977620b541122c027cbbb5376cfd0a0aa80c3993
Humberto Pereira <humberto_pereira@live.com>
2023-12-17 15:51:37 +0100

    first commit

---
+++ react-components/src/components/ControlledForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class ControlledForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            first
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ControlledForm
