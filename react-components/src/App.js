a593f4c5ab3a1606e800e11577ca6b7abf826eeb
Humberto Pereira <humberto_pereira@live.com>
2023-12-17 19:16:11 +0100

    commit

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import StatefulGreeting from "./components/StatefulGreeting";
-import HooksCounter from "./components/HooksCounter";
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <UncontrolledForm />
     </div>
   );
 }
